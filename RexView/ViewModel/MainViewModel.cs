573b41d9cba911c4a3b50079fc1d3eb38fe311a9
安富 孝司 <koji_micro@hotmail.co.jp>
2019-02-03 12:18:18 +0900

    ViewModel 記述の簡略化

--- RexView/ViewModel/MainViewModel.cs
+++ RexView/ViewModel/MainViewModel.cs
@@ -7,24 +7,14 @@ namespace RexView.ViewModel
 {
     public class MainViewModel : NotifyProperty, INotifyPropertyChanged, IDisposable
     {
-        public ICommand InitializeCommand { get => GetValue<ICommand>(); set => SetValue(value); }
-        public ICommand DisposeCommand { get => GetValue<ICommand>(); set => SetValue(value); }
-        public ICommand ReferenceCommand { get => GetValue<ICommand>(); set => SetValue(value); }
+        public ICommand InitializeCommand { get => GetValue(new ActionCommand(Initialize)); set => SetValue(value); }
+        public ICommand DisposeCommand { get => GetValue(new ActionCommand(Dispose)); set => SetValue(value); }
+        public ICommand ReferenceCommand { get => GetValue(new ActionCommand(Reference)); set => SetValue(value); }
 
-        public string Title { get => GetValue<string>(); set => SetValue(value); }
+        public string Title { get => GetValue("RexView"); set => SetValue(value); }
 
-        public RegexModel Regex { get => GetValue<RegexModel>(); set => SetValue(value); }
-
-        public MainViewModel()
-        {
-            InitializeCommand = new ActionCommand(Initialize);
-            DisposeCommand = new ActionCommand(Dispose);
-            ReferenceCommand = new ActionCommand(Reference);
-            Regex = RegexModel.Load();
-
-            Title = "RexView";
-        }
-
+        public RegexModel Regex { get => GetValue(RegexModel.Load()); set => SetValue(value); }
+        
         private void Initialize()
         {
             Regex.InitializeCommand.Execute(new object());
